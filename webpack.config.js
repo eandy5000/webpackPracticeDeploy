2b917e949b5c3e297ef391cf00d601fd91a48d6d
Andy Eccleston <eandy5000@yahoo.com>
2017-02-21 15:08:34 -0600

    used code-splitting to put vendors in a seperate package

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 var webpack = require('webpack');
 var path = require('path');
+var VENDOR_LIBS = [
+'faker', 'react', 'lodash', 'redux', 'react-redux', 'react-router','redux-form',
+'redux-thunk', 'react-input-range'
+];
+
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    })
+  ]
 };
